7ed2b9b7f20d06e006d0baa9b23a1b11228435cb
nickh10 <nickh10@gmail.com>
2015-06-04 14:53:30 -0700

    got skins working with very basic controller and styling. Need to improve

--- mvc/controllers/champController.js
+++ mvc/controllers/champController.js
@@ -1,6 +1,11 @@
 lolCalc.controller("champCtrl", ["$scope", "$q", "$routeParams", "$location", "lolService", 
 	function($scope, $q, $routeParams, $location, lolService, dataService) {
+
+		var self = this;
+		var skins = [];
+		var skinIndex = 0;
 		$scope.keys = ["Q","W","E","R"];
+
 		this.init = function(){
 			$scope.loaded = false;
 			lolService.getVersions()
@@ -14,6 +19,7 @@ lolCalc.controller("champCtrl", ["$scope", "$q", "$routeParams", "$location", "l
 				$scope.loaded = true;
 				console.log(data);
 				$scope.data = data;
+				self.generateSkinUrls(data.key, data.skins);
 			});
 		};
 		this.init();
@@ -22,11 +28,39 @@ lolCalc.controller("champCtrl", ["$scope", "$q", "$routeParams", "$location", "l
             $location.path("Champions/"+champ);
         };
 
-        $scope.switchTo = function(currentTab) {
-        	if(currentTab == "skins" && typeof $scope.skinUrl === "undefined"){
-        		
+        $scope.changeImage = function(change) {
+        	if(change == 'prev'){
+        		skinIndex -= 1;
+        		$scope.skin = skins[skinIndex];
+        		if(skinIndex == 0){
+        			$scope.prevDis = true;
+        		}
+        		if(skinIndex == skins.length-2){
+        			$scope.nextDis = false;
+        		}
         	}
-        	$scope.currentTab = currentTab;
+        	else {
+        		skinIndex += 1;
+        		$scope.skin = skins[skinIndex];
+        		if(skinIndex == skins.length-1){
+        			$scope.nextDis = true;
+        		}
+        		if(skinIndex == 1){
+        			$scope.prevDis = false;
+        		}
+        	}
+        };
+
+        this.generateSkinUrls = function(champName, skinInfo){
+        	window.skinInfo = skinInfo;
+        	for (var i = 0; i < skinInfo.length; i++){
+        		var temp = {};
+        		temp['name'] = skinInfo[i].name;
+        		temp['url'] = "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+champName+"_"+skinInfo[i].num+".jpg";
+        		skins.push(temp);
+        	}
+        	$scope.skin = skins[0];
+        	$scope.prevDis = true;
         };
 	}
 ]);
